e1287fd90437fbf462fa17aecc6281a30ce2e0be
wegar2 <awegrzyn17@gmail.com>
2026-02-12 22:34:39 +0100

    added configuration of logging

--- pynbp/__init__.py
+++ pynbp/__init__.py
@@ -9,3 +9,20 @@ __all__ = [
     "get_gold_prices",
     "get_interest_rates_table"
 ]
+
+import logging
+
+def configure_logging():
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+
+configure_logging()
